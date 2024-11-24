1490a3944d785097955381df3e21bb9d6847a83c
biruk-tafese <biruk.tafesse-ug@aau.edu.et>
2024-11-25 02:19:00 +0300

    first commit

---
+++ lib/pages/onboarding_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatefulWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/logo.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
